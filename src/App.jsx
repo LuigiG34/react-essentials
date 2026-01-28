ebf00268483db9fa7e08b5d5ed5c5f3f0161c991
Luigi Gandemer <luigigandemer6@gmail.com>
2026-01-28 16:00:44 +0100

    Storing components styles next to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { CORE_CONCEPTS } from "./data.js";
 
-import Header from "./components/Header";
-import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 
 function App() {
   return (
     <div>
         <Header />
-      <main>
-          <section id="core-concepts">
-              <h2>Core Concepts</h2>
-              <ul>
-                  <CoreConcept {...CORE_CONCEPTS[0]} />
-                  <CoreConcept {...CORE_CONCEPTS[1]} />
-                  <CoreConcept {...CORE_CONCEPTS[2]} />
-                  <CoreConcept
-                      title={CORE_CONCEPTS[3].title}
-                      description={CORE_CONCEPTS[3].description}
-                      image={CORE_CONCEPTS[3].image}
-                  />
-              </ul>
-          </section>
-      </main>
+        <main>
+            <section id="core-concepts">
+                <h2>Core Concepts</h2>
+                <ul>
+                    <CoreConcept {...CORE_CONCEPTS[0]} />
+                    <CoreConcept {...CORE_CONCEPTS[1]} />
+                    <CoreConcept {...CORE_CONCEPTS[2]} />
+                    <CoreConcept
+                        title={CORE_CONCEPTS[3].title}
+                        description={CORE_CONCEPTS[3].description}
+                        image={CORE_CONCEPTS[3].image}
+                    />
+                </ul>
+            </section>
+        </main>
     </div>
   );
 }
