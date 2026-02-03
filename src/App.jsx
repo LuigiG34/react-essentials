4b0593ea7e9f703e1fc5c105bae1a4199ca673cd
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-03 10:33:31 +0100

    Passing functions as value to props

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,42 @@ import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
-  return (
-    <div>
-        <Header />
-        <main>
-            <section id="core-concepts">
-                <h2>Core Concepts</h2>
-                <ul>
-                    <CoreConcept {...CORE_CONCEPTS[0]} />
-                    <CoreConcept {...CORE_CONCEPTS[1]} />
-                    <CoreConcept {...CORE_CONCEPTS[2]} />
-                    <CoreConcept
-                        title={CORE_CONCEPTS[3].title}
-                        description={CORE_CONCEPTS[3].description}
-                        image={CORE_CONCEPTS[3].image}
-                    />
-                </ul>
-            </section>
+    function handleSelect() {
+        console.log('Hello World !');
+    }
 
-            <section id="examples">
-                <h2>Examples</h2>
-                <menu>
-                    <TabButton>Components</TabButton>
-                    <TabButton>JSX</TabButton>
-                    <TabButton>Props</TabButton>
-                    <TabButton>State</TabButton>
-                </menu>
-            </section>
-        </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept
+                            title={CORE_CONCEPTS[3].title}
+                            description={CORE_CONCEPTS[3].description}
+                            image={CORE_CONCEPTS[3].image}
+                        />
+                    </ul>
+                </section>
+
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={handleSelect}>Components</TabButton>
+                        <TabButton onSelect={handleSelect}>JSX</TabButton>
+                        <TabButton onSelect={handleSelect}>Props</TabButton>
+                        <TabButton onSelect={handleSelect}>State</TabButton>
+                    </menu>
+
+                    Dynamic Content
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
