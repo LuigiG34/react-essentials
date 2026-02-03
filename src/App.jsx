d9fd87011a8810090dd6bd6a783872d242d57fe0
Luigi Gandemer <luigigandemer6@gmail.com>
2026-02-03 11:11:14 +0100

    State and Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
+    let [ selectedTopic, setSelectedTopic ] = useState("components");
+
     function handleClick(selectedButton) {
-        console.log(selectedButton);
+        setSelectedTopic(selectedButton);
     }
 
     return (
@@ -26,7 +30,6 @@ function App() {
                         />
                     </ul>
                 </section>
-
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
@@ -35,8 +38,13 @@ function App() {
                         <TabButton onClick={() => handleClick('props')}>Props</TabButton>
                         <TabButton onClick={() => handleClick('state')}>State</TabButton>
                     </menu>
-
-                    Dynamic Content
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </pre>
+                    </div>
                 </section>
             </main>
         </div>
